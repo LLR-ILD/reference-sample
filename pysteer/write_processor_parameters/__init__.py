f88e862c0c0e8efd6e0d31d80cd90a2c36b22a0f
kunathj <jonas.kunath@gmx.de>
2020-02-06 19:31:28 +0100

    Progress on the steering python class

---
+++ pysteer/write_processor_parameters/__init__.py
@@ -0,0 +1,5 @@
+from . import extract_from_cpp
+from . import processor_paths
+from . import write_dict
+from .extract_from_cpp import marlin_processors_dict
+from .write_dict import update_registered
